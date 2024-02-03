3c985479172296fc6087b3ed7601576908f16f00
Jakub Kawecki <jakub@forloop.ai>
2024-02-03 17:41:58 +0100

    FE:change: config changes

--- backend/config.py
+++ backend/config.py
@@ -18,3 +18,74 @@ class Config(BaseSettings):
 @lru_cache
 def get_config() -> Config:
     return Config()
+
+
+LOGGING_CONFIG = {
+    'disable_existing_loggers': False,
+    'formatters': {
+        'access_file': {
+            '()': 'uvicorn.logging.AccessFormatter',
+            'fmt': '%(levelprefix)s %(asctime)s %(client_addr)s %(request_line)s %(status_code)s',
+            'use_colors': False,
+        },
+        'access_stream': {
+            '()': 'uvicorn.logging.AccessFormatter',
+            'fmt': '%(levelprefix)s %(asctime)s %(client_addr)s %(request_line)s %(status_code)s',
+            'use_colors': True,
+        },
+        'default_file': {
+            '()': 'uvicorn.logging.DefaultFormatter',
+            'fmt': '%(levelprefix)s %(message)s',
+            'use_colors': False,
+        },
+        'default_stream': {
+            '()': 'uvicorn.logging.DefaultFormatter',
+            'fmt': '%(levelprefix)s %(message)s',
+            'use_colors': True,
+        },
+    },
+    'handlers': {
+        'requests_to_file': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'access_file',
+            'filename': 'logs/requests.log',
+            'maxBytes': 10240,
+            'backupCount': 10,
+        },
+        'requests_to_stream': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'access_stream',
+            'stream': 'ext://sys.stdout',
+        },
+        'errors_to_file': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'default_file',
+            'filename': 'logs/internal.log',
+            'maxBytes': 10240,
+            'backupCount': 10,
+        },
+        'errors_to_stream': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default_stream',
+            'stream': 'ext://sys.stderr',
+        },
+    },
+    'loggers': {
+        'uvicorn.access': {
+            'handlers': ['requests_to_file', 'requests_to_stream'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'uvicorn': {
+            'handlers': ['errors_to_file', 'errors_to_stream'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'uvicorn.error': {
+            'handlers': ['errors_to_file', 'errors_to_stream'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+    'version': 1,
+}
