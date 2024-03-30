02fec0667e113e0ce454d2d609d3edccaa376a51
Jakub Kawecki <jakub@forloop.ai>
2024-03-30 20:25:13 +0100

    FE:new: add Icon component

---
+++ frontend/src/components/Icon.tsx
@@ -0,0 +1,31 @@
+import { Stack } from "react-bootstrap";
+import Tooltip from "../components/Tooltip";
+
+type IconProps = {
+    symbol: string;
+    value?: string | number;
+    tooltip: string;
+    iconSize?: 1 | 2 | 3 | 4 | 5 | 6;
+    gap?: number;
+    placement?: "top" | "bottom" | "left" | "right";
+    className?: string;
+};
+
+export default function Icon({
+    symbol,
+    value = "",
+    tooltip,
+    iconSize = 2,
+    gap = 1,
+    placement = "bottom",
+    className = "",
+}: IconProps) {
+    return (
+        <Tooltip content={tooltip} placement={placement}>
+            <Stack direction="horizontal" gap={gap} className={className}>
+                <span className={`material-symbols-outlined fs-${iconSize}`}>{symbol}</span>
+                <span className="fs-5">{value}</span>
+            </Stack>
+        </Tooltip>
+    );
+}
