ac24e8b8c6911a83ebcfdbc808920578dda7b471
Jakub Kawecki <jakub@forloop.ai>
2024-04-27 13:22:25 +0200

    FE:fix: correct countdown logic in `CountDownTimer`

--- frontend/src/components/CountdownTimer.tsx
+++ frontend/src/components/CountdownTimer.tsx
@@ -13,25 +13,27 @@ export default function CountdownTimer({
     className,
     precisionDigit = 1,
 }: CountdownTimerProps) {
-    const end_date = new Date(Date.now() + time * 1000);
     const [countdown, setCountdown] = useState(time);
+    const end_date = start_date
+        ? new Date(new Date(start_date).getTime() + time * 1000)
+        : new Date(Date.now() + time * 1000);
 
     useEffect(() => {
-        const timerId = setInterval(
-            () => {
-                setCountdown((_) => {
-                    const time_left = end_date.getTime() - Date.now();
-                    if (time_left <= 0) {
-                        clearInterval(timerId);
-                        return 0;
-                    } else {
-                        return time_left / 1000;
-                    }
-                });
-            },
-            1000 / 10 ** precisionDigit,
-        );
-    }, [time, start_date]);
+        setCountdown(time);
+        const timerId = setInterval(() => {
+            setCountdown((_) => {
+                const time_left = end_date.getTime() - Date.now();
+                if (time_left <= 0) {
+                    clearInterval(timerId);
+                    return 0;
+                } else {
+                    return time_left / 1000;
+                }
+            });
+        }, 1000 / 10 ** precisionDigit);
 
+        // Return a cleanup function that clears the interval
+        return () => clearInterval(timerId);
+    }, [time, start_date, end_date, precisionDigit]);
     return <div className={className}>{countdown.toFixed(precisionDigit)}</div>;
 }
