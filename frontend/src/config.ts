b445a3f4a3df36eb4f3c74977d9063618c660dd5
Jakub Kawecki <jakub@forloop.ai>
2024-04-28 21:17:20 +0200

    FE:new: clean up `WordList` component, set up config constants for multiple params

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -11,3 +11,7 @@ if (typeof _WEBSOCKET_URL === "undefined") {
 export const WEBSOCKET_URL = _WEBSOCKET_URL;
 
 export const CHAT_MESSAGE_LIMIT = 30;
+export const WORD_LIST_LENGTH = 6;
+export const WORD_LIST_MAX_WORD_SIZE = 1.75; // rem
+export const TURN_START_DELAY = 1;
+export const GAME_START_DELAY = 1;
