14addcbbf2d25b9690da54e1f3ca1a0b1ef83d83
Jakub Kawecki <jakub@forloop.ai>
2024-05-22 21:42:04 +0200

    FE:rwk: Redo `RulesDescription`

--- frontend/src/components/RulesDescription.tsx
+++ frontend/src/components/RulesDescription.tsx
@@ -2,47 +2,53 @@ import Accordion from "react-bootstrap/Accordion";
 import ListGroup from "react-bootstrap/ListGroup";
 
 import Bubble from "./Bubble";
+import Icon from "./Icon";
 
 export default function RulesDescription() {
+    const itemClassName = "px-0 py-2 d-flex align-items-center";
+
     return (
         <Bubble>
             <Accordion flush>
                 <Accordion.Item eventKey="0">
-                    <Accordion.Header>Rules</Accordion.Header>
+                    <Accordion.Header>Deathmatch - rules</Accordion.Header>
                     <Accordion.Body className="p-0">
-                        <ListGroup variant="flush" className="m-0">
-                            <ListGroup.Item className="px-0 py-1">
+                        <ListGroup variant="flush" className="mx-2">
+                            <ListGroup.Item className={itemClassName}>
                                 Words must be sourced from a standard English dictionary
                             </ListGroup.Item>
-                            <ListGroup.Item className="px-0 py-1">
-                                Words must consist of minimum 3 letters
+                            <ListGroup.Item className={itemClassName}>
+                                Each word must start with the last letter of the previous word -
+                                either correct or wrong
                             </ListGroup.Item>
-                            <ListGroup.Item className="px-0 py-1">
-                                Each word must start with the last letter of the previous word
+                            <ListGroup.Item className={itemClassName}>
+                                Once used word cannot be repeated in the same game
                             </ListGroup.Item>
-                            <ListGroup.Item className="px-0 py-1">
-                                No word repetitions, no proper nouns, no abbreviations, no slang
+                            <ListGroup.Item className={itemClassName}>
+                                Each player has a
+                                <Icon symbol="history" iconSize={4} inline className="mx-1" />
+                                time limit to provide a word
                             </ListGroup.Item>
-                            <ListGroup.Item className="px-0 py-1">
-                                There's a time limit of 10 seconds per player per turn
+                            <ListGroup.Item className={itemClassName}>
+                                Each players starts a game with
+                                <Icon symbol="start" iconSize={4} inline className="mx-1" />
+                                points
                             </ListGroup.Item>
-                            <ListGroup.Item className="px-0 py-1">
-                                Exceeding the 10 - second time limit loses you 5 points
+                            <ListGroup.Item className={itemClassName}>
+                                Repeated, invalid word or timeout results in subtraction of
+                                <Icon symbol="dangerous" iconSize={4} inline className="mx-1" />
+                                points
                             </ListGroup.Item>
-                            <ListGroup.Item className="px-0 py-1">
-                                Repeating a word already used in the current game loses you 5 points
+                            <ListGroup.Item className={itemClassName}>
+                                Correct answer earns you
+                                <Icon symbol="check" iconSize={4} inline className="mx-1" />
+                                points
                             </ListGroup.Item>
-                            <ListGroup.Item className="px-0 py-1">
-                                Using an invalid word loses you 5 points
+                            <ListGroup.Item className={itemClassName}>
+                                You lose a game when your points fall below 0
                             </ListGroup.Item>
-                            <ListGroup.Item className="px-0 py-1">
-                                Being the last remaining player after all others are eliminated
-                            </ListGroup.Item>
-                            <ListGroup.Item className="px-0 py-1">
-                                Correct answer earns you 2 points
-                            </ListGroup.Item>
-                            <ListGroup.Item className="px-0 py-1">
-                                You lose when your points fall below 0
+                            <ListGroup.Item className={itemClassName}>
+                                The last player standing is the winner
                             </ListGroup.Item>
                         </ListGroup>
                     </Accordion.Body>
