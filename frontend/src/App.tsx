d23a776996ff07420e3fe03fc682177b060d532e
Jakub Kawecki <jakub@forloop.ai>
2024-05-24 19:36:55 +0200

    FE:rwk: redo NotAuthorizedError handling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Stack from "react-bootstrap/Stack";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Chat from "./components/Chat";
+import { ConnectionErrorBoundary } from "./components/ErrorBoundaries";
 import GenericModal from "./components/GenericModal";
 import Header from "./components/Header";
 import { LoginModal } from "./components/NewPlayerModal";
@@ -14,49 +15,51 @@ import LobbyPage from "./pages/LobbyPage";
 import RoomPage from "./pages/RoomPage";
 
 export default function App() {
-    const { loggedPlayer: player, modalConfigs } = useStore();
+    const { setLoggedPlayer, loggedPlayer: player, modalConfigs } = useStore();
 
     return (
-        <BrowserRouter>
-            {player === null ? (
-                <LoginModal />
-            ) : (
-                <>
-                    {modalConfigs.roomRules && (
-                        <RoomRulesModal
-                            disabledFields={modalConfigs.roomRules.disabledFields}
-                            defaultValues={modalConfigs.roomRules.defaultValues}
-                            onSubmit={modalConfigs.roomRules.onSubmit}
-                        />
-                    )}
-                    {modalConfigs.generic && (
-                        <GenericModal
-                            title={modalConfigs.generic.title}
-                            body={modalConfigs.generic.body}
-                        />
-                    )}
-                    <Header />
-                    <Container fluid className="p-3">
-                        <WebSocketProvider>
-                            <Stack gap={3}>
-                                <Container>
-                                    <div className="text-center fs-6">
-                                        Use this code to log into your account again
-                                    </div>
-                                    <div className="text-center mt-2 fs-5">{player?.id}</div>
-                                </Container>
-                                <RulesDescription />
-                                <Routes>
-                                    <Route path="/" element={<LobbyPage />} />
-                                    <Route path="/room" element={<RoomPage />} />
-                                    <Route path="*" element={<Navigate to="/" />} />
-                                </Routes>
-                                <Chat />
-                            </Stack>
-                        </WebSocketProvider>
-                    </Container>
-                </>
-            )}
-        </BrowserRouter>
+        <ConnectionErrorBoundary setLoggedPlayer={setLoggedPlayer}>
+            <BrowserRouter>
+                {player === null ? (
+                    <LoginModal />
+                ) : (
+                    <>
+                        {modalConfigs.roomRules && (
+                            <RoomRulesModal
+                                disabledFields={modalConfigs.roomRules.disabledFields}
+                                defaultValues={modalConfigs.roomRules.defaultValues}
+                                onSubmit={modalConfigs.roomRules.onSubmit}
+                            />
+                        )}
+                        {modalConfigs.generic && (
+                            <GenericModal
+                                title={modalConfigs.generic.title}
+                                body={modalConfigs.generic.body}
+                            />
+                        )}
+                        <Header />
+                        <Container fluid className="p-3">
+                            <WebSocketProvider>
+                                <Stack gap={3}>
+                                    <Container>
+                                        <div className="text-center fs-6">
+                                            Use this code to log into your account again
+                                        </div>
+                                        <div className="text-center mt-2 fs-5">{player?.id}</div>
+                                    </Container>
+                                    <RulesDescription />
+                                    <Routes>
+                                        <Route path="/" element={<LobbyPage />} />
+                                        <Route path="/room" element={<RoomPage />} />
+                                        <Route path="*" element={<Navigate to="/" />} />
+                                    </Routes>
+                                    <Chat />
+                                </Stack>
+                            </WebSocketProvider>
+                        </Container>
+                    </>
+                )}
+            </BrowserRouter>
+        </ConnectionErrorBoundary>
     );
 }
--- frontend/src/components/ErrorBoundaries.tsx
+++ frontend/src/components/ErrorBoundaries.tsx
@@ -1,4 +1,6 @@
-import React, { createRef, ErrorInfo, ReactNode } from "react";
+import React, { ErrorInfo, ReactNode } from "react";
+
+import { AuthError } from "../errors";
 
 interface ErrorBoundaryState {
     hasError: boolean;
@@ -6,35 +8,27 @@ interface ErrorBoundaryState {
 
 interface ErrorBoundaryProps {
     children: ReactNode;
+    setLoggedPlayer: (player: null) => void;
 }
 
 export class ConnectionErrorBoundary extends React.Component<
     ErrorBoundaryProps,
     ErrorBoundaryState
 > {
-    ref: React.RefObject<HTMLDivElement>;
-
     constructor(props: ErrorBoundaryProps) {
         super(props);
-        // this.state = { hasError: false };
-        this.ref = createRef<HTMLDivElement>();
-    }
-
-    static getDerivedStateFromError(_: Error): ErrorBoundaryState {
-        // Update state so the next render will show the fallback UI.
-        return { hasError: true };
     }
 
     componentDidCatch(error: Error, info: ErrorInfo): void {
-        this.ref.current?.append(error.toString());
+        // this.ref.current?.append(error.toString());
+
+        // If the error is an AuthError, call the logOut function
+        if (error instanceof AuthError) {
+            this.props.setLoggedPlayer(null);
+        }
     }
 
     render(): ReactNode {
-        return (
-            <>
-                {this.props.children}
-                <div className="error" ref={this.ref}></div>
-            </>
-        );
+        return <>{this.props.children}</>;
     }
 }
