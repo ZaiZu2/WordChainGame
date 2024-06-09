806f13a89f69bb7186adbc8c54b72498558b395e
Jakub Kawecki <jakub@forloop.ai>
2024-06-09 23:29:35 +0200

    BE:next: Add TLS certificates

--- frontend/docker-entrypoint.bash
+++ frontend/docker-entrypoint.bash
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 set -eu
 
-# Replace environment variables in the Nginx config file
-envsubst '${BACKEND_HOST} ${BACKEND_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+# Replace the environment variables in the Nginx configuration file
+envsubst '${BACKEND_HOST} ${BACKEND_PORT} ${DOMAIN}' \
+< /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
-exec "$@"
+# Install certbot and get the SSL certificate
+apt-get update
+apt-get install -y certbot
+certbot certonly --standalone --noninteractive --agree-tos --email ${CERTBOT_EMAIL} \
+-d ${DOMAIN} -d www.${DOMAIN}
+certbot renew --dry-run
+
+nginx -g 'daemon off;'
