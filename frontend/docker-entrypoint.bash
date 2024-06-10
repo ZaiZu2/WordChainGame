0b4b69b1b1ca0e59811bd2073b18eda5c123dee2
Jakub Kawecki <jakub@forloop.ai>
2024-06-11 00:38:29 +0200

    DPL:new:  Use certbot plugin for nginx

--- frontend/docker-entrypoint.bash
+++ frontend/docker-entrypoint.bash
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 set -eu
 
-# Replace the environment variables in the Nginx configuration file
+# Run nginx with replaced environment variables in the Nginx configuration file
 envsubst '${BACKEND_HOST} ${BACKEND_PORT} ${DOMAIN}' \
 < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+nginx &
+
 
 # Install certbot and get the SSL certificate
 apt-get update
-apt-get install -y certbot
-certbot certonly --standalone --noninteractive --agree-tos --email ${CERTBOT_EMAIL} \
+apt-get install -y certbot python3-certbot-nginx
+certbot --nginx --noninteractive --agree-tos --email ${CERTBOT_EMAIL} \
 -d ${DOMAIN} -d www.${DOMAIN}
 certbot renew --dry-run
 
+# Reload Nginx to apply the new certificates
+nginx -s reload
 nginx -g 'daemon off;'
