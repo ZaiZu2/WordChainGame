0cd7b9d86e14f8ca856825ffc6eea85b01a3fea3
Jakub Kawecki <jakub@forloop.ai>
2024-06-10 23:21:34 +0200

    gxfxfg

--- frontend/docker-entrypoint.bash
+++ frontend/docker-entrypoint.bash
@@ -5,7 +5,7 @@ set -eu
 envsubst '${BACKEND_HOST} ${BACKEND_PORT} ${DOMAIN}' \
 < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
-wget google.com
+curl google.com
 
 # Install certbot and get the SSL certificate
 apt-get update
