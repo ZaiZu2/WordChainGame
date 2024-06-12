20d6031f7aa1c8b82015f22217479c771c427daf
Jakub Kawecki <jakub@forloop.ai>
2024-06-12 21:40:34 +0200

    awerf

--- frontend/docker-entrypoint.bash
+++ frontend/docker-entrypoint.bash
@@ -6,16 +6,10 @@ envsubst '${BACKEND_HOST} ${BACKEND_PORT} ${DOMAIN}' \
 < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 nginx &
 
+certbot --nginx --noninteractive --agree-tos --email ${CERTBOT_EMAIL} -d ${DOMAIN} -d www.${DOMAIN}
+certbot renew --dry-run
+echo "0 12 * * * /usr/bin/certbot renew --quiet" | crontab -
 
-# # Install certbot and get the SSL certificate
-# apt-get update
-# apt-get install -y certbot python3-certbot-nginx cron
-# certbot --nginx --noninteractive --agree-tos --email ${CERTBOT_EMAIL} \
-# -d ${DOMAIN} -d www.${DOMAIN}
-# certbot renew --dry-run
-# echo "0 12 * * * /usr/bin/certbot renew --quiet" | crontab -
-
-sleep 5 # Wait to ensure nginx has fully started
 nginx -s stop # Stop the background nginx, used by certbot to issue certificates
 sleep 5 # Wait to ensure nginx has fully stopped
 nginx -g 'daemon off;' # Start foreground nginx, which is the main container process
